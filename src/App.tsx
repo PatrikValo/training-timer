e1e25040e0f83ec193d72be96485278d83094476
Patrik Valo <xvalo2@fi.muni.cz>
2021-06-18 00:44:42 +0200

    Add support for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ClockPage from "./pages/clockPage";
 import {
   Container,
@@ -9,10 +9,20 @@ import {
   makeStyles,
   Theme,
   createStyles,
+  MuiThemeProvider,
+  createMuiTheme,
+  Switch,
+  CssBaseline,
 } from "@material-ui/core";
 import SettingsIcon from "@material-ui/icons/Settings";
-import { getTotalTime, storeTotalTime } from "./core/storage";
+import {
+  getIsLight,
+  getTotalTime,
+  storeIsLight,
+  storeTotalTime,
+} from "./core/storage";
 import SettingDialog from "./components/settingDialog";
+import { indigo } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,10 +32,41 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const ourLightTheme = {
+  palette: {
+    primary: {
+      main: "#2196f3",
+    },
+    secondary: {
+      main: "#d32f2f",
+    },
+  },
+};
+
+const ourDarkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#2196f3",
+    },
+    secondary: {
+      main: "#d32f2f",
+    },
+    background: {
+      default: "rgba(0, 0, 0, 0.87)",
+    },
+  },
+});
+
 function App() {
   const classes = useStyles();
   const [open, setOpen] = useState<boolean>(false);
   const [totalTime, setTotalTime] = useState<number>(getTotalTime());
+  const [isLight, setIsLight] = useState<boolean>(getIsLight());
+
+  const muiTheme = isLight
+    ? createMuiTheme(ourLightTheme)
+    : createMuiTheme(ourDarkTheme);
 
   const handleClose = () => {
     setOpen(false);
@@ -40,30 +81,47 @@ function App() {
     setTotalTime(getTotalTime());
   };
 
+  const handleChangeTheme = () => {
+    storeIsLight(!isLight);
+    setIsLight(getIsLight());
+  };
+
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Timer
-          </Typography>
-          <div>
-            <IconButton color="inherit" aria-label="menu" onClick={handleOpen}>
-              <SettingsIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container style={{ height: "calc(100vh - 150px)" }}>
-        <ClockPage totalTime={totalTime} />
-      </Container>
-      <SettingDialog
-        open={open}
-        totalTime={totalTime}
-        onClose={handleClose}
-        onChange={handleChange}
-      />
-    </div>
+    <MuiThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Timer
+            </Typography>
+            <div>
+              <Switch
+                checked={isLight}
+                color="default"
+                onChange={handleChangeTheme}
+              />
+              <IconButton
+                color="inherit"
+                aria-label="menu"
+                onClick={handleOpen}
+              >
+                <SettingsIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container style={{ height: "calc(100vh - 150px)" }}>
+          <ClockPage totalTime={totalTime} />
+        </Container>
+        <SettingDialog
+          open={open}
+          totalTime={totalTime}
+          onClose={handleClose}
+          onChange={handleChange}
+        />
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,10 +1,26 @@
-const key = "totalTime";
+const keyTotalTime = "totalTime";
 
 export function getTotalTime(): number {
-  const result = localStorage.getItem(key);
+  const result = localStorage.getItem(keyTotalTime);
   return result ? +result : 7 * 60_000 + 30_000;
 }
 
 export function storeTotalTime(totalTime: number): void {
-  localStorage.setItem(key, totalTime + "");
+  localStorage.setItem(keyTotalTime, totalTime + "");
+}
+
+const keyTheme = "theme";
+const light = "light";
+const dark = "dark";
+
+export function getIsLight(): boolean {
+  const result = localStorage.getItem(keyTheme);
+  if (result) {
+    return result === light ? true : false;
+  }
+  return true;
+}
+
+export function storeIsLight(isLight: boolean): void {
+  localStorage.setItem(keyTheme, isLight ? light : dark);
 }
